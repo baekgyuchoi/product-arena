a98738e62d0a4f6b90bd0eac65e93e03270c61e4
tonychoi3580 <tony.baekgyu.choi@gmail.com>
2024-05-13 00:40:16 -0700

    Update ProductNamePopover.tsx

--- src/components/products/ProductNamePopover.tsx
+++ src/components/products/ProductNamePopover.tsx
@@ -21,8 +21,8 @@ const ProductNamePopover: React.FC<ProductNamePopoverProps> = (props) => {
                 <PopoverTrigger>
                     <span className=" text-center italic">{product_name.slice(0,50) + "..."}</span>
                 </PopoverTrigger>
-                <PopoverContent className=''>
-                    <span className="text-lg ">{product_name}</span>
+                <PopoverContent className='w-screen'>
+                    <span className="text-lg w-screen lg:w-full ">{product_name}</span>
                 </PopoverContent>
             </Popover>
         </div>
